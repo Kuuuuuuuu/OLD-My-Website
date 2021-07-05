1995134d13452b3bbd43a5d93c4d74d1387fcc35
MelidaZ <69802296+MelidaZ@users.noreply.github.com>
2021-07-06 06:07:17 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+document.onclick = () => applyCursorRippleEffect(event);
+
+function applyCursorRippleEffect(e) {
+   const ripple = document.createElement("div");
+  
+   ripple.className = "ripple";
+   document.body.appendChild(ripple);
+
+  ripple.style.left = `${e.clientX}px`;
+  ripple.style.top = `${e.clientY}px`; 
+   ripple.style.animation = `ripple-effect .4s  linear`;
+   ripple.onanimationend = () => {
+     document.body.removeChild(ripple);
+     
+   }
+  
+}
+
+// extra and optional part:
+
+const all = document.body.getElementsByTagName("*");
+for (var i = 0;  i < all.length; ++i) {
+  all[i].onclick = (event) => event.stopPropagation();
+}
