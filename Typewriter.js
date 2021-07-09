bf63934b59f249022288afced7ce172c5449765c
MelidaZ <69802296+MelidaZ@users.noreply.github.com>
2021-07-09 09:57:29 +0700

    Create Typewriter.js

---
+++ Typewriter.js
@@ -0,0 +1,11 @@
+var i = 0;
+var txt = 'Complexity Network';
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
