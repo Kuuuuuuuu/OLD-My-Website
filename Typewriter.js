7790765ca5bbf6829c2058b34a7da28956ed3f32
MelidaZ <69802296+MelidaZ@users.noreply.github.com>
2021-07-14 18:30:29 +0700

    Update Typewriter.js

--- Typewriter.js
+++ Typewriter.js
@@ -1,7 +1,7 @@
 var i = 0;
 var txt = 'Complexity Network';
 var speed = 50;
-document.load = () => typeWriter(event);
+GlobalEventHandlers.onload = () => typeWriter(event);
 
 function typeWriter() {
   if (i < txt.length) {
