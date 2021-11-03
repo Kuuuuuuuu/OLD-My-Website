f854eb3f70ceed7d3b7bda12942c0075b344b1e2
KohakuChan <69802296+MelidaZ@users.noreply.github.com>
2021-11-03 21:54:40 +0700

    Create clock.js

---
+++ clock.js
@@ -0,0 +1,15 @@
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
+  setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
