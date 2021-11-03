3d13d4ac11d0e70c4de3594b94c3ea00e66ac2de
KohakuChan <69802296+MelidaZ@users.noreply.github.com>
2021-11-04 00:21:43 +0700

    Update cps.js

--- cps.js
+++ cps.js
@@ -2,42 +2,41 @@ let box = document.querySelector(".box");
 let time = document.querySelector(".time");
 let clicks = document.querySelector(".clicks");
 let click = 0;
+ler sec = 10;
 
 let clickElement = document.createElement("div");
 clickElement.setAttribute("class","box");
 
 let btn = document.querySelector(".btn");
 btn.style.display = "none";
+
+function loop() {
+  setInterval(() => {
+    if(sec !== 0) {
+       sec--;
+    } else {
+      time.innerHTML = "Time over";
+      box.style.display = "none";
+      btn.style.display = "inline-block";
+      btn.innerHTML = "Play again";
+      btn.addEventListener("click",function(){ location.reload(); })
+      time.style.display = "none";
+      clicks.innerHTML = "";
+      document.querySelector("h1").innerHTML = `Your click speed is ${click/5} CPS<br>${click} clicks in 5 seconds `;
+      clickElement.replaceWith(box);
+      click = 0;
+      sec = 10;
+     }
+  }, 1000);
+}
+
 function stopwatch(){
-    setTimeout(() => {
-        time.innerHTML = "5 Seconds Left";
-        setTimeout(() => {
-            time.innerHTML = "4 Seconds Left";
-            setTimeout(() => {
-                time.innerHTML = "3 Seconds Left";
-                setTimeout(() => {
-                    time.innerHTML = "2 Seconds Left";
-                    setTimeout(() => {
-                        time.innerHTML = "1 Seconds Left";
-                        setTimeout(() => {
-                            time.innerHTML = "Time over";
-                            box.style.display = "none";
-                            btn.style.display = "inline-block";
-                            btn.innerHTML = "Play again";
-                            btn.addEventListener("click",function(){
-                                location.reload();
-                            })
-                            time.style.display = "none";
-                            clicks.innerHTML = "";
-                            document.querySelector("h1").innerHTML = `Your click speed is ${click/5} CPS<br>${click} clicks in 5 seconds `;
-                            clickElement.replaceWith(box);
-                            click = 0;  
-                        }, 20);
-                    }, 1000);
-                }, 1000);
-            }, 1000);
-        }, 1000);
-    }, 0);
+  if(sec == 10) {
+     loop();
+  }
+  setInterval(() => {
+    time.innerHTML = "${sec} Seconds Left";
+  }, 1000);
 }
 
 box.addEventListener('click',function(){
@@ -48,7 +47,7 @@ box.addEventListener('click',function(){
     btn.style.display = "none";
     click = 0;
     clickElement.addEventListener('click',function(){
-       document.querySelector("h1").innerHTML = `Your click speed is ${click/5} CPS<br>${click}Clicks`;
+       document.querySelector("h1").innerHTML = `Your click speed is ${click/5} CPS<br>${click} Clicks`;
        click++;
        clicks.innerHTML = click;
     })
