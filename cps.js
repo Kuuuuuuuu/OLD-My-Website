c6415a786ea75bcdbedc6f5e51b10b675fbaa863
KohakuChan <69802296+MelidaZ@users.noreply.github.com>
2021-11-04 00:44:09 +0700

    Update cps.js

--- cps.js
+++ cps.js
@@ -1,19 +1,18 @@
-let box = document.querySelector(".box");
-let time = document.querySelector(".time");
-let click = 0;
-let sec = 5;
+const clickElement = document.createElement("div");
+const box = document.querySelector(".box");
+const time = document.querySelector(".time");
+const btn = document.querySelector(".btn");
+var click = 0;
+var sec = 5;
 
-let clickElement = document.createElement("div");
 clickElement.setAttribute("class","box");
-
-let btn = document.querySelector(".btn");
 btn.style.display = "none";
 
 function loop() {
   setInterval(() => {
     if(sec !== 0) {
        sec--;
-    } else {
+    } else if(sec == 0) {
       time.innerHTML = "Time over";
       box.style.display = "none";
       btn.style.display = "inline-block";
@@ -25,26 +24,21 @@ function loop() {
       click = 0;
       sec = 5;
      }
-  }, 1000);
-}
+   }, 900);
+ }
 
-function stopwatch(){
-  if(sec == 5) {
-     loop();
-  }
-  setInterval(() => {
-    time.innerHTML = `${sec} Seconds Left`;
-  }, 1);
-}
+ function stopwatch(){
+   loop();
+   setInterval(() => {
+     time.innerHTML = `${sec} Seconds Left`;
+     document.querySelector("h1").innerHTML = `Your click speed is ${click/5} CPS<br>${click} Clicks`;
+   }, 1);
+ }
 
-box.addEventListener('click',function(){
+ box.addEventListener('click',function(){
     stopwatch();
-    click = 1;
     box.replaceWith(clickElement);
     btn.style.display = "none";
-    click = 0;
-    clickElement.addEventListener('click',function(){
-       document.querySelector("h1").innerHTML = `Your click speed is ${click/5} CPS<br>${click} Clicks`;
-       click++;
-    })
+    clickElement.addEventListener('click',function(){ click++; })
 })
+//Nah please not use this code it bug he
