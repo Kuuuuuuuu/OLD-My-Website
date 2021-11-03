1be312ac3bb6142019f5efa52e8b424049a7b403
KohakuChan <69802296+MelidaZ@users.noreply.github.com>
2021-11-04 01:44:15 +0700

    Create tictactoe.js

---
+++ tictactoe.js
@@ -0,0 +1,94 @@
+const checkbox = document.getElementById('flexSwitchCheckDefault');
+    checkbox.addEventListener('change', function () {
+        if (this.checked) {
+            document.body.style.backgroundColor = "#1c1c1c";
+            document.body.style.color = "#c9d1d9";
+        } else {
+            document.body.style.backgroundColor = "white";
+            document.body.style.color = "black";
+        }
+    })
+
+       async function clickEvent(id) {
+            const button = document.getElementById(id);
+            if (button.innerText === "⠀") {
+                button.innerText = "X";
+                let buttons = [];
+                for (let i = 0; i < 9; i++) {
+                    const button = document.getElementById("btn-" + i);
+                    buttons[i.toString()] = button.innerText;
+                }
+                let winner = await calculateWinner(buttons);
+                if (winner != null) {
+                    if (winner === "X") {
+                        Swal.fire('You win!').then(function () {
+                            clearBoxes()
+                        });
+                    } else {
+                        Swal.fire('You lose!').then(function () {
+                            clearBoxes()
+                        });
+                    }
+                    return;
+                }
+                for (let i = 0; i < 9; i++) {
+                    const button = document.getElementById("btn-" + i);
+                    if (button.innerText === "⠀") {
+                        button.innerText = "O";
+                        break;
+                    }
+                }
+                buttons = [];
+                for (let i = 0; i < 9; i++) {
+                    const button = document.getElementById("btn-" + i);
+                    buttons[i.toString()] = button.innerText;
+                }
+                winner = await calculateWinner(buttons);
+                if (winner != null) {
+                    if (winner === "X") {
+                        Swal.fire('You win!').then(function () {
+                            clearBoxes()
+                        });
+                    } else {
+                        Swal.fire('You lose!').then(function () {
+                            clearBoxes()
+                        });
+                    }
+                    return;
+                }
+                if (!buttons.includes("⠀")) {
+                    Swal.fire('Tie!').then(function () {
+                        clearBoxes()
+                    });
+                }
+            }
+        }
+
+        async function calculateWinner(squares) {
+            const lines = [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8],
+                [0, 4, 8],
+                [2, 4, 6]
+            ];
+            for (let i = 0; i < lines.length; i++) {
+                const [a, b, c] = lines[i];
+                if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                    if (squares[a] !== "⠀") {
+                        return squares[a];
+                    }
+                }
+            }
+            return null;
+        }
+
+        function clearBoxes() {
+            for (let i = 0; i < 9; i++) {
+                const button = document.getElementById("btn-" + i);
+                button.innerText = "⠀";
+            }
+        }
