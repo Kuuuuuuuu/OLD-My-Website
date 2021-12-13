4bf92c121bb9c18fd50cb2ed53f001d33411688c
KohakuChan <69802296+MelidaZ@users.noreply.github.com>
2021-12-13 23:10:43 +0700

    daddda

--- Script/Typewriter.js
+++ Script/Typewriter.js
@@ -1,18 +1,6 @@
 let i = 0;
 const txt = 'KohakuChan';
-const txt2 = 'Social Infos';
 const speed = 100;
-
-// actually this is lazy coding
-// but I'm too lazy to make a new function
-
-function SocialWriter() {
-    if (i < txt2.length) {
-        document.getElementById('writer2').innerHTML += txt2.charAt(i);
-        i++;
-        setTimeout(SocialWriter, speed);
-    }
-}
 
 function typeWriter() {
     if (i < txt.length) {
