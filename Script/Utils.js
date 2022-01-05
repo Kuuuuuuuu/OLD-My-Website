c5023e8a3610189a80495bf1b225175a74a57f1d
KohakuChan <69802296+MelidaZ@users.noreply.github.com>
2022-01-05 18:26:57 +0700

    dwdwdwddd

--- Script/Utils.js
+++ Script/Utils.js
@@ -5,10 +5,8 @@ document.onclick = () => applyCursorRippleEffect(event);
 
 function applyCursorRippleEffect(e) {
     const ripple = document.createElement("div");
-
     ripple.className = "ripple";
     document.body.appendChild(ripple);
-
     ripple.style.left = `${e.clientX}px`;
     ripple.style.top = `${e.clientY}px`;
     ripple.style.animation = `ripple-effect .4s  linear`;
