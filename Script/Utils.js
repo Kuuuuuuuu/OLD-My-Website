b1212e63a900a940256dae38c16bb377fff6c513
MelidaZ <notkungz.suphakorn@gmail.com>
2022-01-08 19:15:09 +0700

    Oops forgot this

--- Script/Utils.js
+++ Script/Utils.js
@@ -49,7 +49,7 @@ function Dashboard() {
     fetch('http://84f9-2405-9800-b921-1fd2-c8fa-402c-1eb4-22f3.ap.ngrok.io').then(response => {
         response.json().then(data => {
             console.log(data);
-            return document.getElementById('count').innerHTML = `Retro Community Online: ${data.presence_count} Members`;
+            return document.getElementById('dashboard').innerHTML = `Retro Community Status: Total Member: ${data.all} / Bots: ${data.bot} / Verify Count: ${data.verify} / Not Verify Count: ${data.notverify}`;
         });
     })
 }
